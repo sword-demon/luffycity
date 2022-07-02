541457ac5738bb1a0b47004a4ac102bdea7a0fd0
wxvirus <1418667580@qq.com>
2022-07-02 23:20:49 +0800

    feature:添加Header和Footer组件和样式以及前端资源

--- luffycityweb/src/views/Home.vue
+++ luffycityweb/src/views/Home.vue
@@ -1,17 +1,13 @@
 <script setup>
-import { reactive } from "vue";
-const store = reactive({
-    value2: null,
-    colors: ["#99A9BF", "#F7BA2A", "#FF9900"],
-});
+import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
 </script>
 
 <template>
-    <h1>首页</h1>
-    <el-row class="mb-4">
-        <el-button type="primary">Primary</el-button>
-        <el-rate v-model="store.value2" :colors="store.colors"></el-rate>
-    </el-row>
+    <div class="home">
+        <Header />
+        <Footer />
+    </div>
 </template>
 
 <style scoped></style>
